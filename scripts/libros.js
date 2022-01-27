a1e331e9ee83a7f506ad4466fd8961d990adba86
David Charif <davidcharif@hotmail.com>
2022-01-27 12:37:24 -0500

    script

--- scripts/libros.js
+++ scripts/libros.js
@@ -1,20 +1,50 @@
-import {url as endpoint} from "./url.js"
-console.log(endpoint);
+import {url as endpoint} from "../api/url.js"
+
+
+let contenedor = document.getElementById("libros")
+const addLibro = async () => {
+    let libro = addLibros()
+    await fetch(endpoint, { 
+    method: 'POST',
+    body: JSON.stringify(objeto),
+    headers: {
+        "Content-Type" : "application/json; charset=utf-8"
+    }
+})}
 const getLibros = async () => {
     const data = await fetch(endpoint);
     const libros = await data.json();
     libros.forEach(libro => {
-        const {titulo} = libro;
-        console.log('titulo', titulo);
+        const {titulo,urlImg,descripcion,precio} = libro;
+        console.log(titulo);
+        contenedor.innerHTML += `
+        <div class="card" style="width: 18rem;">
+        <img src="${urlImg}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${titulo}</h5>
+          <p class="card-text">${precio}</p>
+          <p class="card-text">${descripcion}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+        `
     });
 }
-const addLibros = () => {
+let buttonAdd = document.getElementById("agregar");
+buttonAdd.addEventListener("click",addLibro)
+
+
+export const addLibros = () => {
+    //recolectar datos
     const nombre = document.getElementById("nombre")
     const descripcion = document.getElementById("descripcion")
-    const url = document.getElementById("url")
+    const urlImg = document.getElementById("urlImg")
     const autor = document.getElementById("autor")
     const editorial = document.getElementById("editorial")
     const precio = document.getElementById("precio")
+    //Crear array
+    let contenedor = {nombre,descripcion,urlImg,autor,editorial,precio};
+    return contenedor;
 }
 const removeLibro = () => {}
 const updateLibros = () => {}
